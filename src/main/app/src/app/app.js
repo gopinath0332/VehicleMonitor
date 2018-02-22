dc7e4d4f0037a96903874a7e9d248f85ba8ce566
Gopinath Rajendran <goprajen@cisco.com>
2018-02-23 02:25:25 +0530

    Adding Page not found

--- src/main/app/src/app/app.js
+++ src/main/app/src/app/app.js
@@ -3,16 +3,6 @@ import React, {Component} from "react";
 import Header from "../components/view/Header";
 import Main from "../components/view/Main";
 
-/*export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header/>
-                <Main/>
-            </div>
-        );
-    }
-}*/
 const App = () => (
     <div>
         <Header/>
--- src/main/app/src/components/view/Main.js
+++ src/main/app/src/components/view/Main.js
@@ -3,14 +3,15 @@ import {Switch, Route} from 'react-router-dom';
 
 import Login from "../login/Login";
 import Dashboard from "../dashboard/Dashboard";
-
-const Main1 = () => (
+import PageNotFound from "../view/PageNotFound";
+const Main = () => (
     <main>
         <Switch>
             <Route exact path='/' component={Login}/>
             <Route path='/dashboard' component={Dashboard}/>
+            <Route path='/*' component={PageNotFound}/>
         </Switch>
     </main>
 );
 
-export default Main1;
+export default Main;
