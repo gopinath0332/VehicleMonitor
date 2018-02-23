f856eb95bf7e9128afe40673ebd09a1186ba2a6e
Gopinath Rajendran <goprajen@cisco.com>
2018-02-23 15:33:06 +0530

    implementing authentication

--- src/main/java/com/spring/rest/DemoController.java
+++ src/main/java/com/spring/rest/DemoController.java
@@ -1,10 +1,17 @@
 package com.spring.rest;
 
+import com.sun.deploy.net.HttpRequest;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/api")
@@ -13,5 +20,13 @@ public class DemoController {
     @ResponseBody
     public String getDummy(ModelMap map) {
         return "Rest Controller is working!!!!";
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public String Login (Model model, HttpServletRequest request){
+//        model.addAttribute("isAuthenticated",true);
+        HttpSession session = request.getSession(false);
+        session.setAttribute("isAuthenticated",true);
+        return "success";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,11 +3,16 @@
 <!DOCTYPE html>
 <html>
 <body>
-<% boolean isAuthenticated = false;%>
+<%
+    boolean isAuthenticated = false;
+    if( session.getAttribute("isAuthenticated") != null)
+        isAuthenticated = (Boolean) session.getAttribute("isAuthenticated");
+%>
 <% if(isAuthenticated){ %>
 <%@include file="./resources/index.html"%>
 <% }else{ %>
 <%@include file="./resources/login.html" %>
 <% } %>
+
 </body>
 </html>
